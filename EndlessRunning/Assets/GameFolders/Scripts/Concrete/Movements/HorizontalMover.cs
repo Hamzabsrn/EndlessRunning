d87936438162f877e13715f6130d3aba878027aa
Hamzabsrn <hamzabasaran1@hotmail.com>
2023-06-26 01:48:01 +0300

    Created prefab

---
+++ EndlessRunning/Assets/GameFolders/Scripts/Concrete/Movements/HorizontalMover.cs
@@ -0,0 +1,24 @@
+using Controller;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mover
+{
+    public class HorizontalMover
+    {
+        PlayerController _playerController;
+
+        public HorizontalMover(PlayerController playerController)
+        {
+            _playerController = playerController;
+        }
+        public void TickFixed(float horizontal, float moveSpeed)
+        {
+            if (horizontal == 0) return;
+
+            _playerController.transform.Translate(Vector3.right * horizontal * Time.deltaTime * moveSpeed);
+        }
+    }
+
+}
---
+++ EndlessRunning/Assets/GameFolders/Scripts/Concrete/Movements/JumpWithRigidbody.cs
@@ -0,0 +1,26 @@
+using Controller;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mover
+{
+
+    public class JumpWithRigidbody : MonoBehaviour
+    {
+        Rigidbody _rigidbody;
+
+        public JumpWithRigidbody(PlayerController playerController)
+        {
+            _rigidbody = playerController.GetComponent<Rigidbody>();
+        }
+        public void TickFixed(float jumpForce)
+        {
+            // if (_rigidbody.velocity.y != 0) return;
+
+            _rigidbody.velocity = Vector3.zero;
+            _rigidbody.AddForce(Vector3.up * Time.deltaTime * jumpForce);
+
+        }
+    }
+}
